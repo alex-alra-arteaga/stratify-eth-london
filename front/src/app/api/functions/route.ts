da8f56bc2564304c5d12d6f66c594950d4e5004d
xRozzo <david.salvatella.gelpi@gmail.com>
2024-03-16 19:51:04 +0000

    add: strategies + calldata working

--- front/src/app/api/functions/route.ts
+++ front/src/app/api/functions/route.ts
@@ -1,4 +1,5 @@
 import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 import {
     POOL_ABI,
     ERC20_ABI,
@@ -30,12 +31,6 @@ type AssetType = {
     name: string;
 };
 
-type PoolType = {
-    address: string;
-    token0: Token;
-    token1: Token;
-};
-
 type PoolDataInReq = {
     poolAddress: string;
     token0: string; // pass the symbol
@@ -43,7 +38,7 @@ type PoolDataInReq = {
 };
 // ! need to be compatilbe with V3 Route function of uniswap the crate a valid route
 // ! pass this in the order of the trade -> do not need to pass maybe the address just the asset name (in the future maybe -> when we have more abstraction)
-type Request = {
+type Body = {
     Pools: PoolDataInReq[]; // pools to change that assats -> have the swap
     Assets: AssetType[]; // all the assets (manager vault do not change)
     Amount: number; // amount In -> will go from Asset 1 to -> Asset 2 (with possible iteretions in the middle)
@@ -52,17 +47,23 @@ type Request = {
 // crate array of tokens
 // then cratte the pools depending on that
 
-export async function GET(request: Request) {
+export async function POST(request : any) {
+    // handle the request
+    const res : Body = await request.json();
+    console.log("res body", res);
+
+
     const chainId = process.env.CHAIN_ID || 11155111; // localhost
     const privateKey = process.env.PRIVATE_KEY; // the one that will have access to call that function or anyone on the localchain
     if (!privateKey) throw new Error("PRIVATE_KEY not set");
     const wallet = new ethers.Wallet(privateKey);
-    const provider = new ethers.JsonRpcProvider("http://127.0.0.1:8545/"); // set json rpc provider -> alchemy
+    const provider = new ethers.JsonRpcProvider("https://rpc2.sepolia.org"); // set json rpc provider -> alchemy
     const signer = wallet.connect(provider);
+
 
     // -> get the % of token 1 and token 2 -> compare to previous
 
-    const tokens: Token[] = request.Assets.map((asset) => {
+    const tokens: Token[] = res.Assets.map((asset) => {
         return new Token(
             Number(chainId),
             asset.address,
@@ -72,61 +73,75 @@ export async function GET(request: Request) {
         );
     });
 
-    const pools: Contract[] = request.Pools.map((pool) => {
+    const pools: Contract[] = res.Pools.map((pool) => {
         return new ethers.Contract(pool.poolAddress, POOL_ABI, signer);
     });
 
-    // uniswap pools type
-    const pools_uniwasp: Pool[] = pools.map(async (pool, index) => {
-        // now we are getting the type pool
-        const token0: Token = tokens.find(
-            (token) => token.symbol === request.Pools[index].token0
-        );
-        const token1: Token = tokens.find(
-            (token) => token.symbol === request.Pools[index].token1
-        );
-        const poolLiquidity = await pool.liquidity();
-        const poolSlot0Data = await pool.slot0();
+    console.log("PPOOOOOLLSSS", pools);
 
-        return new Pool(
-            token0,
-            token1,
-            FeeAmount.LOW,
-            pool.slot0().toString(),
-            poolLiquidity,
-            Number(poolSlot0Data[1]),
-            [
-                {
-                    index: nearestUsableTick(
-                        TickMath.MIN_TICK,
-                        TICK_SPACINGS[FeeAmount.LOW]
-                    ),
-                    liquidityNet: pool.liquidity().toString(),
-                    liquidityGross: poolLiquidity,
-                },
-                {
-                    index: nearestUsableTick(
-                        TickMath.MAX_TICK,
-                        TICK_SPACINGS[FeeAmount.LOW]
-                    ),
-                    liquidityNet: (
-                        BigInt(poolLiquidity) * BigInt(-1)
-                    ).toString(),
-                    liquidityGross: poolLiquidity.toString(),
-                },
-            ]
-        );
-    });
+    // uniswap pools type
+    const pools_uniwasp: Promise<Pool[]> = Promise.all(
+        pools.map(async (pool, index) => {
+            // now we are getting the type pool
+            console.log("tokens", {tokens});
+            const poolAddress = await pool.target; 
+            console.log("poolAddress", poolAddress);
+            const auxiliarPool = res.Pools.find((pool) => pool.poolAddress == poolAddress);
+            console.log("auxiliarPool", auxiliarPool);
+            const token0: Token | undefined = tokens.find(
+                (token) => token.symbol === auxiliarPool?.token0
+            );
+            if (token0 == undefined) throw new Error("Token 0 not found");
+            const token1: Token | undefined = tokens.find(
+                (token) => token.symbol === auxiliarPool?.token1
+            );
+            if (token1 == undefined) throw new Error("Token 1 not found");
+            const poolLiquidity = await pool.liquidity();
+
+            const poolSlot0Data = await pool.slot0();
+
+            console.log("poolSlot0Data", poolSlot0Data);
+
+            return new Pool(
+                token0,
+                token1,
+                FeeAmount.LOW,
+                poolSlot0Data[0].toString(),
+                poolLiquidity.toString(),
+                Number(poolSlot0Data[1]),
+                [
+                    {
+                        index: nearestUsableTick(
+                            TickMath.MIN_TICK,
+                            TICK_SPACINGS[FeeAmount.LOW]
+                        ),
+                        liquidityNet: (await pool.liquidity()).toString(),
+                        liquidityGross: poolLiquidity.toString(),
+                    },
+                    {
+                        index: nearestUsableTick(
+                            TickMath.MAX_TICK,
+                            TICK_SPACINGS[FeeAmount.LOW]
+                        ),
+                        liquidityNet: (poolLiquidity * BigInt(-1)).toString(),
+                        liquidityGross: poolLiquidity.toString(),
+                    },
+                ]
+            );
+        })
+    );
+
+    console.log("pools_uniwasp",await  pools_uniwasp);
 
     const slippageTolerance = new Percent(50, 10000); // defalut of uniswap -> 0.5% (increment if needed)
     // amoutn in shold be always the tokens[0]
     const parsed_amount_in = CurrencyAmount.fromRawAmount(
         tokens[0],
-        request.Amount.toFixed(0)
+        res.Amount.toFixed(0)
     );
 
     const route = new V3Route(
-        pools_uniwasp,
+        await pools_uniwasp,
         tokens[0],
         tokens[tokens.length - 1]
     ); // create the route
@@ -135,6 +150,8 @@ export async function GET(request: Request) {
         parsed_amount_in,
         TradeType.EXACT_INPUT
     );
+
+    console.log("trade", trade);
 
     // trade in router
     const routerTrade = new UniswapTrade(
@@ -155,32 +172,36 @@ export async function GET(request: Request) {
         { slippageTolerance, recipient: M_VAULT_ADDRESS }
     );
 
+    console.log("routerTrade", routerTrade);
+
     const { calldata, value } = SwapRouter.swapCallParameters(routerTrade);
 
     const inter = new ethers.Interface(VAULT_ABI);
     const Deadline = Math.floor(Date.now() / 1000) + 60 * 20; // 20 minutes from the current Unix time
 
-    const additionalCalldata = inter.encodeFunctionData(
-        "fulfillRequest",
-        [calldata, Deadline, tokens[0].address, parsed_amount_in.toFixed(0)] // token 1 is eth (weth) // token 2 is euro (eurc)
-    );
+    console.log("calldata", calldata);
 
-    const tx = await signer.sendTransaction({
-        data: additionalCalldata,
-        to: M_VAULT_ADDRESS,
-        value: value,
-        from: signer.address,
-    });
+    // const additionalCalldata = inter.encodeFunctionData(
+    //     "fulfillRequest",
+    //     [calldata, Deadline, tokens[0].address, parsed_amount_in.toFixed(0)] // token 1 is eth (weth) // token 2 is euro (eurc)
+    // );
 
-    const recipient = await tx.wait();
-    if (recipient?.status === 1) {
-        console.log("Transaction successful");
-    } else {
-        console.log("Transaction failed");
-        throw new Error(
-            `Transaction failed to: ${recipient?.to} with calldata: ${calldata}`
-        );
-    }
+    // const tx = await signer.sendTransaction({
+    //     data: additionalCalldata,
+    //     to: M_VAULT_ADDRESS,
+    //     value: value,
+    //     from: signer.address,
+    // });
+
+    // const recipient = await tx.wait();
+    // if (recipient?.status === 1) {
+    //     console.log("Transaction successful");
+    // } else {
+    //     console.log("Transaction failed");
+    //     throw new Error(
+    //         `Transaction failed to: ${recipient?.to} with calldata: ${calldata}`
+    //     );
+    // }
 
     return NextResponse.json({ calldata: calldata }, { status: 200 });
 }
