9416701d66766dc8e31a24f934b16cc653e7c331
xRozzo <david.salvatella.gelpi@gmail.com>
2024-03-16 21:29:11 +0000

    add: dependencies

---
+++ front/src/app/strategies/gnosis.ts
@@ -0,0 +1,16 @@
+
+// create a gnosis pay
+// -> front will have the first part of the script to send the first tx -> and maybe approve the second tx?
+// make the script so they can deposit on our contract
+// 
+// get top 5 yields tokens with pools with Eure
+// fixes 
+// make it in the back to test
+
+
+// create gnosis wallet with delay moduel
+// demon on the page with docyumentation
+
+// tx to add to the vault
+// withdraw from the vault back to gnosis pay
+
