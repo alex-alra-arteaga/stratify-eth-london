a625c3d14d5f9838db6d8fcdd2e2cb3ee3e0f353
Javier Ríos <javier.rios-urbano@epitech.eu>
2024-03-17 07:23:02 +0100

    fix: shorten email

--- front/src/components/NavBar.tsx
+++ front/src/components/NavBar.tsx
@@ -47,7 +47,7 @@ export default function NavBar() {
                       <div className="flex flex-row items-center justify-center gap-5 h-10">
                         <div className="w-22 h-auto py-2 px-3 bg-blue-600 text-white rounded-lg">
                           <span>
-                            {data.email}
+                            {data.email!.split("@")[0]}
                           </span>
                         </div>
                         <div
